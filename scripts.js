0e18f033198f6ed10a155d660ef9c950005793b4
Betacraft <hoangkimc9@gmail.com>
2023-12-12 20:46:32 +0700

    Change to forEach

--- scripts.js
+++ scripts.js
@@ -2,19 +2,19 @@ let counter = 0;
 
 const buttons = document.querySelectorAll('.btn');
 
-buttons[0].addEventListener('click', () => {
-  counter--;
-  displayCounter();
-});
-
-buttons[1].addEventListener('click', () => {
-  counter = 0;
-  displayCounter();
-});
-
-buttons[2].addEventListener('click', () => {
-  counter++;
-  displayCounter();
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.innerText === "Decrease") {
+      counter--;
+      displayCounter();
+    } else if (button.innerText === "Reset") {
+      counter = 0;
+      displayCounter();
+    } else {
+      counter++;
+      displayCounter();
+    }
+  });
 });
 
 function displayCounter() {
